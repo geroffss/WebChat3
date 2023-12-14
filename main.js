6f13c88ef169f454539d82a4b34dc8bcb4583fab
Nagyosy Gero <87004307+geroffss@users.noreply.github.com>
2023-12-14 13:59:48 +0200

    v34

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// main.js - Main process handling
 const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 
-function createWindow() {
+function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -11,14 +11,13 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-
   mainWindow.loadFile('index.html');
-
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
-
-
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
 app.on('ready', () => {
@@ -35,4 +34,20 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
 });
