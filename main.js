26c4bb92f0b797e6aea021d274893c668f0f67f7
Nagyosy Gero <87004307+geroffss@users.noreply.github.com>
2023-12-14 14:20:36 +0200

    v36

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
+      preload: 'preload.js'
     },
   });
   mainWindow.loadFile('index.html');
@@ -21,6 +23,7 @@ function createWindow () {
 }
 
 app.on('ready', () => {
+  
   createWindow();
 });
 
